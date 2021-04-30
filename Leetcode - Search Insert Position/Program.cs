c4c25ec779b176959629df9f28601e91d4f309ab
mrgucci1 <dalquist.keaton221@gmail.com>
2021-04-29 21:43:52 -0500

    Add project files.

---
+++ Leetcode - Search Insert Position/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode___Search_Insert_Position
+{
+    class Program
+    {
+        //https://leetcode.com/problems/search-insert-position/
+        static void Main(string[] args)
+        {
+            int[] x = { 1, 3, 5, 6 };
+            int solution = reverseint(x, 5);
+            Console.WriteLine(solution);
+            Console.ReadKey();
+        }
+        static public int reverseint(int[] nums, int target)
+        {
+            var start = 0;
+            var end = nums.Length - 1;
+            var mid = 0;
+
+            while (start <= end)
+            {
+                mid = start + (end - start) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                else if (nums[mid] > target)
+                    end = mid - 1;
+                else start = mid + 1;
+            }
+
+            if (target > nums[mid])
+                return mid + 1;
+
+            return mid;
+        }
+    }
+}
---
+++ Leetcode - Search Insert Position/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Leetcode - Search Insert Position")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Leetcode - Search Insert Position")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("209a61a1-a6bb-4b3a-ab8e-2417117a0fef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
